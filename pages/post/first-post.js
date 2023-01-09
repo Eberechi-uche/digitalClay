8ace2c37bcf01b1cd7e654bd178b5a358c1bb323
Richard Eberechi Uche <eb3rechi@gmail.com>
2023-01-09 07:07:17 +0100

    add css styling

--- pages/post/first-post.js
+++ pages/post/first-post.js
@@ -1,20 +1,32 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../component/layout";
 
 function FirstPost() {
   return (
-    <div>
-      <h1>my first next js post</h1>
-      <Head></Head>
-    </div>
+    <>
+      <Head>
+        <title> welcome to my first post</title>
+      </Head>
+      <div>
+        <Layout>
+          <h1>my first next js post</h1>
+          <Header title={"click counter the making"}></Header>
+          <Header title={"prgraming as a service"}></Header>
+          <Header title={"irrational thought"}></Header>
+          <Header title={"glass Mind"}></Header>
+        </Layout>
+      </div>
+    </>
   );
 }
 
 export default FirstPost;
 
-function Head() {
+function Header({ title }) {
   return (
     <>
-      <h2>recent posts</h2>
+      <h2>{title}</h2>
       <hr></hr>
       <p>this is a tale of a software dev</p>
       <Link href="/"> home</Link>
